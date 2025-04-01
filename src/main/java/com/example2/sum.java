8576358af94e84238e95b31f4a025c2ae013c21f
varshini663 <uvarsh59@gmail.com>
2025-04-01 11:58:53 +0530

    initial commit

---
+++ src/main/java/com/example2/sum.java
@@ -0,0 +1,10 @@
+package com.example2;
+public class sum{
+public static void main(String[] args)
+{
+int a=5;
+int b=10;
+int sum=a+b;
+System.out.println("The sum of 2 numbers is"+sum);
+}
+}
---
+++ src/main/java/com/example3/subtraction.java
@@ -0,0 +1,9 @@
+package com.example3;
+public class subtraction{
+public static void main(String args[]){
+int a=30;
+int b=5;
+int subt=a-b;
+System.out.println("Subtraction of two numbers:"+subt);
+}
+}
